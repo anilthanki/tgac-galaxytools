e35b83a71838d4442f60e2c0f8f5e21fc8dcc336
anilthanki <thanki.anil@gmail.com>
2018-01-15 16:17:08 +0000

    adds an option for BLAST Score ratio with ensembl version 1 and 2

--- tools/blast_parser/blast_parser.py
+++ tools/blast_parser/blast_parser.py
@@ -6,6 +6,9 @@ import argparse
 import math
 import sqlite3
 import tempfile
+from time import gmtime, strftime
+print(strftime("%Y-%m-%d %H:%M:%S", gmtime()))
+
 
 BATCH_SIZE = 2000
 
@@ -16,7 +19,13 @@ def create_tables(conn):
         id INTEGER PRIMARY KEY,
         sequence1_id VARCHAR NOT NULL,
         sequence2_id VARCHAR NOT NULL,
-        weight INTEGER NOT NULL)''')
+        weight DOUBLE NOT NULL,
+        score DOUBLE NOT NULL,
+        sequence1_self_score DOUBLE NOT NULL,
+        sequence2_self_score DOUBLE NOT NULL,
+        blast_score_ratio DOUBLE NOT NULL,
+        reciprocal BOOLEAN NOT NULL
+        )''')
     conn.commit()
 
 
@@ -26,7 +35,10 @@ def main():
     parser.add_argument('-o', metavar='out-file', type=argparse.FileType('wt'), required=True, help='Path to output file')
     parser.add_argument('-r', action='store_true', default=False,
                         dest='reciprocal',
-                        help='Annotate homolog pair')
+                        help='Returns only reciprocal results')
+    parser.add_argument('-e', default=0,
+                        dest='ensembl',
+                        help='Returns results which are either reciprocal or having BLAST Score Ratio more than 0.33')
     options = parser.parse_args()
 
     db_file = tempfile.NamedTemporaryFile(suffix=".sqlite")
@@ -46,11 +58,12 @@ def main():
         sequence2_id = line_cols[1]
 
         # Ignore self-matching hits
-        if sequence1_id == sequence2_id:
-            continue
+        # if sequence1_id == sequence2_id:
+            # continue
 
         i = i + 1
         evalue = float(line_cols[10])
+
 
         # Convert evalue to an integer weight with max 100
         if evalue != 0.0:
@@ -59,12 +72,14 @@ def main():
             # If the evalue is 0, leave weight at 100
             weight = 100
 
-        cur.execute('INSERT INTO alignment (id, sequence1_id, sequence2_id, weight) VALUES (?, ?, ?, ?)',
-                    (i, sequence1_id, sequence2_id, weight))
+        score = line_cols[13]
+        cur.execute('INSERT INTO alignment (id, sequence1_id, sequence2_id, weight, score, blast_score_ratio, sequence1_self_score, sequence2_self_score, reciprocal) VALUES (?, ?, ?, ?, ?, ?, ? ,? ,?)',
+                    (i, sequence1_id, sequence2_id, weight, score, 0, 0, 0, 0))
 
         # Commit transaction at every BATCH_SIZE rows to save memory
         if i % BATCH_SIZE == 0:
             conn.commit()
+
     # Commit final transaction
     conn.commit()
     options.i.close()
@@ -73,8 +88,33 @@ def main():
     cur.execute('DELETE FROM alignment WHERE id NOT IN (SELECT id FROM alignment GROUP BY sequence1_id, sequence2_id HAVING weight=max(weight))')
     conn.commit()
 
-    if options.reciprocal:
-        query = 'SELECT a1.sequence1_id, a1.sequence2_id, a1.weight FROM alignment a1, alignment a2 WHERE a1.sequence1_id = a2.sequence2_id AND a1.sequence2_id = a2.sequence1_id ORDER BY a1.id'
+    # Find sequence1_self_score
+    cur.execute('UPDATE alignment SET sequence1_self_score = (SELECT a.score FROM alignment a WHERE a.sequence1_id = alignment.sequence1_id and a.sequence1_id = a.sequence2_id)')
+    conn.commit()
+
+    # Find sequence2_self_score
+    cur.execute('UPDATE alignment SET sequence2_self_score = (SELECT a.score FROM alignment a WHERE a.sequence1_id = alignment.sequence2_id and a.sequence1_id = a.sequence2_id)')
+    conn.commit()
+
+     # Delete self matching alignments
+    cur.execute('DELETE FROM alignment WHERE sequence1_id = sequence2_id')
+    conn.commit()
+
+    # Calculate BLAST Score ratio
+    cur.execute('UPDATE alignment SET blast_score_ratio = score/MAX(sequence1_self_score, sequence2_self_score)')
+    conn.commit()
+
+    # Updating Reciprocal results
+    cur.execute('UPDATE alignment SET reciprocal = 1 where id in (SELECT a1.id FROM alignment a1, alignment a2 WHERE a1.sequence1_id = a2.sequence2_id AND a1.sequence2_id = a2.sequence1_id ORDER BY a1.id)')
+    conn.commit()
+
+    if options.ensembl:
+        if options.ensembl is "1":
+            query = 'SELECT sequence1_id, sequence2_id, weight FROM alignment WHERE reciprocal = 1 OR blast_score_ratio > 0.33'
+        elif options.ensembl is "2":
+            query = 'SELECT sequence1_id, sequence2_id, weight FROM alignment WHERE blast_score_ratio > 0.11'
+    elif options.reciprocal:
+        query = 'SELECT sequence1_id, sequence2_id, weight FROM alignment WHERE reciprocal = 1'
     else:
         query = 'SELECT sequence1_id, sequence2_id, weight FROM alignment ORDER BY id'
 
@@ -89,6 +129,7 @@ def main():
     conn.close()
     db_file.close()
     options.o.close()
+    print(strftime("%Y-%m-%d %H:%M:%S", gmtime()))
 
 
 if __name__ == "__main__":
